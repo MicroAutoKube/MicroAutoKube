beafd02f1c24935511f2e2deb094ca176dfaba80
mewpk <milzaza123@gmail.com>
2025-02-17 23:39:24 +0700

    refactor: remove unused Prisma import and user data fetching from Home component

--- dashboard-autokube/src/app/page.tsx
+++ dashboard-autokube/src/app/page.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
-import { prisma } from "@/lib/prisma";
+
 
 export default async function Home() {
-  const data = await prisma.user.findMany();
+  
   return (
     <div>
-      {data.map(user => (
-        <div key={user.id}>{user.name}</div>
-      ))}
     </div>
   );
 }
