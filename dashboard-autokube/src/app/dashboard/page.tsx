e7adbdc2176db4d76aad64d92dc4846e08412aa9
montagan <bitoey2016@gmail.com>
2025-03-05 16:07:34 +0700

    feat: add ListProfile component to dashboard page

--- dashboard-autokube/src/app/dashboard/page.tsx
+++ dashboard-autokube/src/app/dashboard/page.tsx
@@ -1,15 +1,14 @@
 import { BackgroundLooper } from '@/components/background'
 import React from 'react'
+import { ListProfile } from "@/components/cluster"
+
 
 const Page = () => {
 
   return (
 
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="border-2 border-[#696796] rounded-md shadow-md p-12 flex flex-col items-center gap-4 bg-gray-900">
-        
-      </div>
-      <BackgroundLooper />
+        <ListProfile />
     </div>
   )
 }
