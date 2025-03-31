2199ee8f5fb12cada2b6d427b9c1997c5de356e2
mewpk <psk.mewpk@gmail.com>
2025-03-31 23:55:32 +0700

    feat: initialize Next.js build artifacts and configuration files

---
+++ dashboard-autokube/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/mewpk/Desktop/Project/MicroAutoKube/dashboard-autokube/src/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
